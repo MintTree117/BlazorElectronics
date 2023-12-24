38b45b016f0f86d311ad2d9d45022ace5e365c5b
MintTree117 <mail@martingrof97.com>
2023-12-24 10:48:12 -0500

    COnnection string again

--- Server/Data/DapperContext.cs
+++ Server/Data/DapperContext.cs
@@ -4,16 +4,20 @@ namespace BlazorElectronics.Server.Data;
 
 public class DapperContext
 {
+    ILogger<DapperContext> Logger;
     readonly IConfiguration _config;
     readonly string _connectionString;
 
-    public DapperContext( IConfiguration config )
+    public DapperContext( IConfiguration config, ILogger<DapperContext> logger )
     {
         _config = config;
+        Logger = logger;
         _connectionString = config.GetConnectionString( "DefaultConnection" ) ?? string.Empty;
+        
+        Logger.LogError( "COnn : " + _connectionString );
+        Logger.LogWarning( "COnn : " + _connectionString );
     }
-
-    public SqlConnection CreateConnection() => new SqlConnection( _connectionString );
+    
     public async Task<SqlConnection> GetOpenConnection()
     {
         var connection = new SqlConnection( _connectionString );
